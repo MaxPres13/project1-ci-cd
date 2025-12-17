4977dae3bedc2f0b056884907aed61ae9f435702
Max <maximilianpres@MAC-TID001298.local>
2025-12-17 09:35:52 +0100

    use docker login action instead of azure cli (docker login via cli not possible)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
 		<div className="wrapper">
 			<h1>CD Prozess eingeführt (Test)</h1>{" "}
 			<h2>Testing mit vitest hinzugefügt</h2>
+			<h3>Deployment Flow geändert von DockerHub zu ACR</h3>
 		</div>
 	);
 }
