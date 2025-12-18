0072becaa1dce1a11965212dd29181b36c02b155
Max <maximilianpres@MAC-TID001298.local>
2025-12-18 13:03:30 +0100

    send frontend text update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ function App() {
 			<h1>CD Prozess eingeführt (Test)</h1>{" "}
 			<h2>Testing mit vitest hinzugefügt</h2>
 			<h3>Deployment Flow geändert von DockerHub zu ACR</h3>
-			<h4>Staging Stand</h4>
+			<h4>Staging Stand (Swap aus Stanging mit Production)</h4>
+			<h4>Terraform Import from Azure Environment </h4>
 		</div>
 	);
 }
