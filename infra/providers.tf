080272e354685ad20bc571a99cf91061a5822770
Max <maximilianpres@MAC-TID001298.local>
2025-12-19 13:17:22 +0100

    move local terraform state to remote backend azure storage account

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,14 +1,21 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 4.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "project1-rg"
+    storage_account_name = "project1tfstate"
+    container_name       = "terraform-state"
+    key                  = "project1.tfstate"
+  }
 
   required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
